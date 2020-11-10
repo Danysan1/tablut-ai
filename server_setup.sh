b6e384bc519131a56d193aa6cd440ed743f0f9b8
Daniele Santini <danysan95@gmail.com>
2020-11-10 23:05:55 +0100

    Migliorato server setup

--- server_setup.sh
+++ server_setup.sh
@@ -1,23 +1,34 @@
 #!/bin/sh
 
-echo 'Installing dependencies...'
-sudo apt update
-sudo apt install openjdk-8-jdk -y
-sudo apt install ant -y
+echo 'Installing dependencies...';
+sudo apt update;
+#sudo apt install openjdk-8-jdk ant git tmux -y || sudo apt install openjdk-11-jdk ant git tmux -y;
+sudo apt install openjdk-8-jdk ant git -y || sudo apt install openjdk-11-jdk ant git -y;
+ret=$?;
 
-echo 'Cloning repo...'
-git clone https://github.com/AGalassi/TablutCompetition.git
-cd TablutCompetition/Tablut
+if [ $ret -eq 0 ]; then
+	echo 'Cloning repo...';
+	git clone https://github.com/AGalassi/TablutCompetition.git;
+	cd TablutCompetition/Tablut;
 
-echo 'Compiling...'
-ant clean
-ant compile
+	echo 'Compiling...';
+	ant clean;
+	ant compile;
 
-echo 'Starting Server...'
-ant server
+	#echo 'Starting Server...';
+	#ant server;
 
-echo 'Testing random white player...'
-ant randomwhite
+	#echo 'Testing random white player...';
+	#ant randomwhite;
 
-echo 'Testing random black player...'
-ant randomblack
+	#echo 'Testing random black player...';
+	#ant randomblack;
+
+	echo 'To start the server:          cd TablutCompetition/Tablut && ant server';
+	echo 'To start random white player: cd TablutCompetition/Tablut && ant randomwhite';
+	echo 'To start random black player: cd TablutCompetition/Tablut && ant randomblack';
+	return 0;
+else
+	echo "ERROR: INSTALLATION FAILED WITH CODE $ret";
+	return 1;
+fi
