6777c16519e3308c731a55d300f6c3c3c63acd2d
Daniele Santini <danysan95@gmail.com>
2020-11-11 20:17:27 +0100

    Fix server_setup

--- server_setup.sh
+++ server_setup.sh
@@ -2,11 +2,13 @@
 
 echo 'Installing dependencies...';
 sudo apt update;
-#sudo apt install openjdk-8-jdk ant git tmux -y || sudo apt install openjdk-11-jdk ant git tmux -y;
 sudo apt install openjdk-8-jdk ant git -y || sudo apt install openjdk-11-jdk ant git -y;
 ret=$?;
 
-if [ $ret -eq 0 ]; then
+if [ $ret -neq 0 ]; then
+	echo "ERROR: INSTALLATION FAILED WITH CODE $ret";
+	return 1;
+elif [ $1 -eq 'original' ]; then
 	echo 'Cloning repo...';
 	git clone https://github.com/AGalassi/TablutCompetition.git;
 	cd TablutCompetition/Tablut;
@@ -27,10 +29,19 @@ if [ $ret -eq 0 ]; then
 	echo 'To start the server:          cd TablutCompetition/Tablut && ant server';
 	echo 'To start random white player: cd TablutCompetition/Tablut && ant randomwhite';
 	echo 'To start random black player: cd TablutCompetition/Tablut && ant randomblack';
-	echo 'To start Tuichu white player: cd TablutCompetition/Tablut && ant tuichuwhite';
-	echo 'To start Tuichu black player: cd TablutCompetition/Tablut && ant tuichublack';
 	return 0;
 else
-	echo "ERROR: INSTALLATION FAILED WITH CODE $ret";
-	return 1;
+	echo 'Cloning repo...';
+	git clone https://github.com/Danysan1/tablut-ai.git;
+	cd Tablut;
+
+	echo 'Compiling...';
+	ant clean;
+	ant compile;
+
+	echo 'To start the server:          cd Tablut && ant server';
+	echo 'To start random white player: cd Tablut && ant randomwhite';
+	echo 'To start random black player: cd Tablut && ant randomblack';
+	echo 'To start Tuichu white player: cd Tablut && ant tuichuwhite';
+	echo 'To start Tuichu black player: cd Tablut && ant tuichublack';
 fi
