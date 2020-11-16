ea4a1774b1e37db456800a6063a727d74095c04b
Daniele Santini <danysan95@gmail.com>
2020-11-16 23:42:42 +0100

    WIP heuristics

--- Tablut/src/edu/tuichu/heuristic/TuichuHeuristic.java
+++ Tablut/src/edu/tuichu/heuristic/TuichuHeuristic.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import it.unibo.ai.didattica.competition.tablut.domain.State;
+import it.unibo.ai.didattica.competition.tablut.domain.State.Pawn;
 import it.unibo.ai.didattica.competition.tablut.domain.State.Turn;
 
 public class TuichuHeuristic extends WeightedHeuristic {
@@ -22,20 +23,20 @@ public class TuichuHeuristic extends WeightedHeuristic {
 		map.put(FactorType.WHITEWIN, Float.valueOf(isWhiteWin(state) ? 1 : 0));
 		map.put(FactorType.BLACKWIN, Float.valueOf(isBlackWin(state) ? 1 : 0));
 		map.put(FactorType.KING_IN_CASTLE, Float.valueOf(isKingInCastle(state) ? 1 : 0));
-		map.put(FactorType.WHITE_PAWNS, getPawns(Turn.WHITE, state));
-		map.put(FactorType.BLACK_PAWNS, getPawns(Turn.BLACK, state));
+		map.put(FactorType.WHITE_PAWNS, getPawns(Pawn.WHITE, state)+getPawns(Pawn.KING, state));
+		map.put(FactorType.BLACK_PAWNS, getPawns(Pawn.BLACK, state));
 		map.put(FactorType.EATEN_WHITE_PAWNS, getEatenPawns(Turn.WHITE, state));
 		map.put(FactorType.EATEN_BLACK_PAWNS, getEatenPawns(Turn.BLACK, state));
 		map.put(FactorType.DISTANCE_TO_WIN, getMinManhattanDistanceFromKingToWin(state));
 		map.put(FactorType.MOVES_TO_WIN, getMinMovesFromKingToWin(state));
-		map.put(FactorType.WHITE_PAWNS_IN_WIN_CELLS, getPawnsInWinCells(Turn.WHITE, state));
-		map.put(FactorType.BLACK_PAWNS_IN_WIN_CELLS, getPawnsInWinCells(Turn.BLACK, state));
-		map.put(FactorType.WHITE_PAWNS_ADJACENT_TO_KING, getPawnsAdjacentToKing(Turn.WHITE, state));
-		map.put(FactorType.BLACK_PAWNS_ADJACENT_TO_KING, getPawnsAdjacentToKing(Turn.BLACK, state));
-		map.put(FactorType.WHITE_PAWNS_IN_KINGS_ROW, getPawnsInKingsRow(Turn.WHITE, state));
-		map.put(FactorType.BLACK_PAWNS_IN_KINGS_ROW, getPawnsInKingsRow(Turn.BLACK, state));
-		map.put(FactorType.WHITE_PAWNS_IN_KINGS_COLUMN, getPawnsInKingsColumn(Turn.WHITE, state));
-		map.put(FactorType.BLACK_PAWNS_IN_KINGS_COLUMN, getPawnsInKingsColumn(Turn.BLACK, state));
+		map.put(FactorType.WHITE_PAWNS_IN_WIN_CELLS, getPawnsInWinCells(Pawn.WHITE, state));
+		map.put(FactorType.BLACK_PAWNS_IN_WIN_CELLS, getPawnsInWinCells(Pawn.BLACK, state));
+		map.put(FactorType.WHITE_PAWNS_ADJACENT_TO_KING, getPawnsAdjacentToKing(Pawn.WHITE, state));
+		map.put(FactorType.BLACK_PAWNS_ADJACENT_TO_KING, getPawnsAdjacentToKing(Pawn.BLACK, state));
+		map.put(FactorType.WHITE_PAWNS_IN_KINGS_ROW, getPawnsInKingsRow(Pawn.WHITE, state));
+		map.put(FactorType.BLACK_PAWNS_IN_KINGS_ROW, getPawnsInKingsRow(Pawn.BLACK, state));
+		map.put(FactorType.WHITE_PAWNS_IN_KINGS_COLUMN, getPawnsInKingsColumn(Pawn.WHITE, state));
+		map.put(FactorType.BLACK_PAWNS_IN_KINGS_COLUMN, getPawnsInKingsColumn(Pawn.BLACK, state));
 		return map;
 	}
 	
@@ -50,13 +51,11 @@ public class TuichuHeuristic extends WeightedHeuristic {
 	}
 
 	protected boolean isKingInCastle(State state) {
-		// TODO To be implemented
-		return false;
+		return state.getPawn(4,4).equals(Pawn.KING);
 	}
 
-	protected int getPawns(Turn playerOwningThePawns, State state) {
-		// TODO To be implemented
-		return 0;
+	protected int getPawns(Pawn playerOwningThePawns, State state) {
+		return state.getNumberOf(playerOwningThePawns);
 	}
 
 	protected int getEatenPawns(Turn playerOwningThePawns, State state) {
@@ -74,22 +73,52 @@ public class TuichuHeuristic extends WeightedHeuristic {
 		return 0;
 	}
 
-	protected int getPawnsInWinCells(Turn playerOwningThePawns, State state) {
+	protected int getPawnsInWinCells(Pawn playerOwningThePawns, State state) {
+		int count = 0;
+		for(int i=1; i<8; i++) {
+			if(
+				state.getPawn(0, i).equals(playerOwningThePawns) ||
+				state.getPawn(8, i).equals(playerOwningThePawns) ||
+				state.getPawn(i, 0).equals(playerOwningThePawns) ||
+				state.getPawn(i, 8).equals(playerOwningThePawns)
+			) count++;
+		}
+		return count;
+	}
+
+	protected int getPawnsAdjacentToKing(Pawn playerOwningThePawns, State state) {
+		int count = 0, x = -1, y = -1;
+		
+		for(int i=0; i<9; i++) {
+			for(int j=0; j<9; j++) {
+				if(state.getPawn(i, j).equals(Pawn.KING)) {
+					x = i;
+					y = j;
+					break;
+				}
+			}
+		}		
+		if(x==-1 || y==-1) {
+			// This should never happen
+			throw new IllegalStateException();
+		}
+		
+		for(int i=(x-1); i<x+1; i++) {
+			for(int j=(y-1); j<(y+1); j++) {
+				if(state.getPawn(i, j).equals(playerOwningThePawns))
+					count++;
+			}
+		}
+		
+		return count;
+	}
+
+	protected int getPawnsInKingsRow(Pawn playerOwningThePawns, State state) {
 		// TODO To be implemented
 		return 0;
 	}
 
-	protected int getPawnsAdjacentToKing(Turn playerOwningThePawns, State state) {
-		// TODO To be implemented
-		return 0;
-	}
-
-	protected int getPawnsInKingsRow(Turn playerOwningThePawns, State state) {
-		// TODO To be implemented
-		return 0;
-	}
-
-	protected int getPawnsInKingsColumn(Turn playerOwningThePawns, State state) {
+	protected int getPawnsInKingsColumn(Pawn playerOwningThePawns, State state) {
 		// TODO To be implemented
 		return 0;
 	}
