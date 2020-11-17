52dbca3d50999342be5b9a469406bcb1fb290564
ParsaD23 <dinoco.parsa23@gmail.com>
2020-11-17 18:35:42 +0100

    minmax algorithm

--- Tablut/src/edu/tuichu/algorithm/MiniMaxAlgorithm.java
+++ Tablut/src/edu/tuichu/algorithm/MiniMaxAlgorithm.java
@@ -1,7 +1,10 @@
 package edu.tuichu.algorithm;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import edu.tuichu.heuristic.TuichuHeuristic;
 import it.unibo.ai.didattica.competition.tablut.domain.Action;
 import it.unibo.ai.didattica.competition.tablut.domain.Game;
 import it.unibo.ai.didattica.competition.tablut.domain.State;
@@ -9,9 +12,16 @@ import it.unibo.ai.didattica.competition.tablut.domain.State.Turn;
 
 public class MiniMaxAlgorithm implements TablutAlgorithm {
 	private Game rules;
-	
+	private TuichuHeuristic heuristic;
+	private Game game;
+	private int depth;
+	private State.Turn player;
+
 	public MiniMaxAlgorithm(Game rules) {
+		/*this.depth = depth;
+		this.player = player;*/
 		this.rules = rules;
+		heuristic = new TuichuHeuristic();
 	}
 
 	@Override
@@ -24,6 +34,50 @@ public class MiniMaxAlgorithm implements TablutAlgorithm {
 			e.printStackTrace();
 		}
 		return a;
+	}
+
+
+	public float minMax(State state, int depth, float alpha, float beta) {
+		Turn turn = state.getTurn();
+		if (depth == 0 || turn == Turn.BLACKWIN || turn == Turn.DRAW || turn == Turn.WHITEWIN) {
+			return evaluate(state);
+		}
+
+		if (player == Turn.WHITE) { // MAXIMIZE
+			float maxEval = Float.NEGATIVE_INFINITY;
+			for (State s : getSuccessors(state, player)) {
+				float eval = minMax(state, depth - 1, alpha, beta);
+				maxEval = Float.max(eval, maxEval);
+				alpha = Float.max(alpha, eval);
+				if (beta <= alpha){
+					break;
+				}
+			}
+			return maxEval;
+		} else if (player == Turn.BLACK) { // MINIMIZE
+			float minEval = Float.POSITIVE_INFINITY;
+			for (State s : getSuccessors(state, player)){
+				float eval = minMax(state, depth - 1, alpha, beta);
+				minEval = Float.min(eval, minEval);
+				beta = Float.min(beta, eval);
+				if (beta <= alpha){
+					break;
+				}
+			}
+			return minEval;
+		}
+		return 0;
+	}
+
+	// TODO remember to change the state after each action
+	private List<State> getSuccessors(State state, Turn player){
+		List<State> states = new ArrayList<>();
+
+		return states;
+	}
+
+	private float evaluate(State state){
+		return heuristic.getValue(state);
 	}
 
 }
