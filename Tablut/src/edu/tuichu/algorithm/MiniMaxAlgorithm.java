01169f7d33d6976925012142099bece819e76674
Jyoti <jyotiyadavaibologna@gmail.com>
2020-11-20 01:48:59 +0530

    going through the code for minmax algorithm changed formatting ctrl+F and added a code in the catch block

--- Tablut/src/edu/tuichu/algorithm/MiniMaxAlgorithm.java
+++ Tablut/src/edu/tuichu/algorithm/MiniMaxAlgorithm.java
@@ -7,7 +7,6 @@ import java.util.List;
 import edu.tuichu.heuristic.TablutHeuristic;
 import edu.tuichu.heuristic.TuichuHeuristic;
 import it.unibo.ai.didattica.competition.tablut.domain.Action;
-import it.unibo.ai.didattica.competition.tablut.domain.Game;
 import it.unibo.ai.didattica.competition.tablut.domain.State;
 import it.unibo.ai.didattica.competition.tablut.domain.State.Pawn;
 import it.unibo.ai.didattica.competition.tablut.domain.State.Turn;
@@ -16,13 +15,13 @@ public class MiniMaxAlgorithm implements TablutAlgorithm {
 	private TablutHeuristic heuristic;
 	private Utilities utilities;
 	private int maxDepth;
-	
+
 	public MiniMaxAlgorithm(int maxDepth, TablutHeuristic heuristic) {
 		this.heuristic = heuristic;
 		this.utilities = new Utilities();
 		this.maxDepth = maxDepth;
 	}
-	
+
 	public MiniMaxAlgorithm(int maxDepth) {
 		this(maxDepth, new TuichuHeuristic());
 	}
@@ -34,34 +33,35 @@ public class MiniMaxAlgorithm implements TablutAlgorithm {
 		return a;
 	}
 
-
 	public Action makeDecision(State state) {
 		List<Action> actions = getPossibleMoves(state);
 		Turn turn = state.getTurn();
 		Action result = null;
 		float evaluation = 0;
-		
-		for (int i = 0; i<actions.size(); i++) {
+
+		for (int i = 0; i < actions.size(); i++) {
 			if (i == 0) {
 				result = actions.get(i);
-				evaluation = minMax(utilities.performMove(state, result), maxDepth, Float.NEGATIVE_INFINITY, Float.POSITIVE_INFINITY);
+				evaluation = minMax(utilities.performMove(state, result), maxDepth, Float.NEGATIVE_INFINITY,
+						Float.POSITIVE_INFINITY);
 			} else {
-				float temp = minMax(utilities.performMove(state, actions.get(i)), maxDepth, Float.NEGATIVE_INFINITY, Float.POSITIVE_INFINITY);
-				if (turn.equals(Turn.WHITE) && evaluation<=temp) {
+				float temp = minMax(utilities.performMove(state, actions.get(i)), maxDepth, Float.NEGATIVE_INFINITY,
+						Float.POSITIVE_INFINITY);
+				if (turn.equals(Turn.WHITE) && evaluation <= temp) {
 					result = actions.get(i);
 					evaluation = temp;
-				} else if (turn.equals(Turn.BLACK) && evaluation>=temp) {
+				} else if (turn.equals(Turn.BLACK) && evaluation >= temp) {
 					result = actions.get(i);
 					evaluation = temp;
-				} 
+				}
 			}
 		}
-		
+
 		return result;
 	}
-	
+
 	public float minMax(State state, int depth, float alpha, float beta) {
-		
+
 		Turn turn = state.getTurn();
 		if (depth == 0 || turn.equals(Turn.BLACKWIN) || turn.equals(Turn.DRAW) || turn.equals(Turn.WHITEWIN)) {
 			return evaluate(state);
@@ -77,50 +77,50 @@ public class MiniMaxAlgorithm implements TablutAlgorithm {
 
 	public float minValue(State state, int depth, float alpha, float beta) {
 		float minEval = Float.POSITIVE_INFINITY;
-		for (State s : getSuccessors(state)){
+		for (State s : getSuccessors(state)) {
 			float eval = maxValue(state, depth - 1, alpha, beta);
 			minEval = Float.min(eval, minEval);
 			beta = Float.min(beta, eval);
-			if (beta <= alpha){
+			if (beta <= alpha) {
 				break;
 			}
 		}
 		return minEval;
 	}
-	
+
 	public float maxValue(State state, int depth, float alpha, float beta) {
 		float maxEval = Float.NEGATIVE_INFINITY;
 		for (State s : getSuccessors(state)) {
 			float eval = minValue(state, depth - 1, alpha, beta);
 			maxEval = Float.max(eval, maxEval);
 			alpha = Float.max(alpha, eval);
-			if (beta <= alpha){
+			if (beta <= alpha) {
 				break;
 			}
 		}
 		return maxEval;
 	}
-	
+
 	private List<State> getSuccessors(State state) {
 		List<Action> actions = getPossibleMoves(state);
 		List<State> successors = new ArrayList<>();
-		
+
 		for (Action a : actions) {
 			successors.add(utilities.performMove(state, a));
 		}
-		
+
 		return successors;
 	}
-	
-	private List<Action> getPossibleMoves(State state){
+
+	private List<Action> getPossibleMoves(State state) {
 		List<Action> actions = new ArrayList<>();
 
 		Pawn[][] board = state.getBoard();
-		
+
 		/* WHITE */
 		if (state.getTurn().equals(Turn.WHITE)) {
-			for (int i = 0; i<9; i++) {
-				for (int j = 0; i<9; i++) {
+			for (int i = 0; i < 9; i++) {
+				for (int j = 0; i < 9; i++) {
 					Pawn pawn = board[i][j];
 					if (pawn.equals(Pawn.KING)) {
 						actions.addAll(getPossibleMoves(state, i, j));
@@ -130,38 +130,38 @@ public class MiniMaxAlgorithm implements TablutAlgorithm {
 				}
 			}
 		}
-		
+
 		/* BLACK */
 		if (state.getTurn().equals(Turn.BLACK)) {
-			for (int i = 0; i<9; i++) {
-				for (int j = 0; i<9; i++) {
+			for (int i = 0; i < 9; i++) {
+				for (int j = 0; i < 9; i++) {
 					Pawn pawn = board[i][j];
 					if (pawn.equals(Pawn.BLACK)) {
 						actions.addAll(getPossibleMoves(state, i, j));
 					}
 				}
 			}
-		}	
+		}
 		return actions;
 	}
 
-	private float evaluate(State state){
+	private float evaluate(State state) {
 		return heuristic.getValue(state);
 	}
-	
+
 	private String getBox(int row, int column) {
 		String ret;
 		char col = (char) (column + 97);
 		ret = col + "" + (row + 1);
 		return ret;
 	}
-	
+
 	private List<Action> getPossibleMoves(State state, int row, int col) {
 		List<Action> result = new ArrayList<>();
 		String from = getBox(row, col);
-		
+
 		// Check all row moves
-		for (int i = 0; i<9; i++) {
+		for (int i = 0; i < 9; i++) {
 			String to = getBox(row, i);
 			try {
 				Action a = new Action(from, to, state.getTurn());
@@ -169,12 +169,12 @@ public class MiniMaxAlgorithm implements TablutAlgorithm {
 					result.add(a);
 				}
 			} catch (IOException e) {
-				
+				e.printStackTrace();
 			}
 		}
-		
+
 		// Check all column moves
-		for (int i = 0; i<9; i++) {
+		for (int i = 0; i < 9; i++) {
 			String to = getBox(i, col);
 			try {
 				Action a = new Action(from, to, state.getTurn());
@@ -182,12 +182,11 @@ public class MiniMaxAlgorithm implements TablutAlgorithm {
 					result.add(a);
 				}
 			} catch (IOException e) {
-				
+				e.printStackTrace();
 			}
 		}
-		
+
 		return result;
 	}
-	
-	
+
 }
