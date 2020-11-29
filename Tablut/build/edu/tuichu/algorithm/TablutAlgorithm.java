fcf495c31498c26975c63ea5e8466a04429e30bc
Daniele Santini <danysan95@gmail.com>
2020-11-29 16:23:24 +0100

    Fix .gitignore

--- Tablut/build/edu/tuichu/algorithm/TablutAlgorithm.java
+++
@@ -1,9 +0,0 @@
-package edu.tuichu.algorithm;
-
-import it.unibo.ai.didattica.competition.tablut.domain.Action;
-import it.unibo.ai.didattica.competition.tablut.domain.State;
-import it.unibo.ai.didattica.competition.tablut.domain.State.Turn;
-
-public interface TablutAlgorithm {
-	public Action getAction(State state);
-}
